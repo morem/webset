4955b8d8ead2c9ab7a24d002b6e1d941ab7e81ec
Lysin Miao <lysin.miao@mstarsemi.com>
2012-06-28 18:07:40 +0800

    1. Update yaml to least

--- html/yaml/add-ons/accessible-tabs/jquery.tabs.js
+++ html/yaml/add-ons/accessible-tabs/jquery.tabs.js
@@ -1,11 +1,11 @@
 /**
  * Accessible Tabs - jQuery plugin for accessible, unobtrusive tabs
  * Build to seemlessly work with the CCS-Framework YAML (yaml.de) not depending on YAML though
- * @requires jQuery - tested with 1.4.2 but might as well work with older versions
+ * @requires jQuery - tested with 1.7 and 1.4.2 but might as well work with older versions
  *
  * english article: http://blog.ginader.de/archives/2009/02/07/jQuery-Accessible-Tabs-How-to-make-tabs-REALLY-accessible.php
  * german article: http://blog.ginader.de/archives/2009/02/07/jQuery-Accessible-Tabs-Wie-man-Tabs-WIRKLICH-zugaenglich-macht.php
- * 
+ *
  * code: http://github.com/ginader/Accessible-Tabs
  * please report issues at: http://github.com/ginader/Accessible-Tabs/issues
  *
@@ -14,8 +14,8 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  *
- * Version: 1.9.1
- * 
+ * Version: 1.9.4
+ *
  * History:
  * * 1.0 initial release
  * * 1.1 added a lot of Accessibility enhancements
@@ -23,17 +23,17 @@
  * * * added check for existing ids on the content containers to use to proper anchors in the tabs
  * * 1.1.1 changed the headline markup. thanks to Mike Davies for the hint.
  * * 1.5 thanks to Dirk Jesse, Ansgar Hein, David Maciejewski and Mike West for commiting patches to this release
- * * * new option syncheights that syncs the heights of the tab contents when the SyncHeight plugin 
+ * * * new option syncheights that syncs the heights of the tab contents when the SyncHeight plugin
  * *   is available http://blog.ginader.de/dev/jquery/syncheight/index.php
  * * * fixed the hardcoded current class
- * * * new option tabsListClass to be applied to the generated list of tabs above the content so lists 
+ * * * new option tabsListClass to be applied to the generated list of tabs above the content so lists
  * *   inside the tabscontent can be styled differently
- * * * added clearfix and tabcounter that adds a class in the schema "tabamount{number amount of tabs}" 
+ * * * added clearfix and tabcounter that adds a class in the schema "tabamount{number amount of tabs}"
  * *   to the ul containg the tabs so one can style the tabs to fit 100% into the width
  * * * new option "syncHeightMethodName" fixed issue: http://github.com/ginader/Accessible-Tabs/issues/2/find
  * * * new Method showAccessibleTab({index number of the tab to show starting with 0})  fixed issue: http://github.com/ginader/Accessible-Tabs/issues/3/find
  * * * added support for the Cursor Keys to come closer to the WAI ARIA Tab Panel Best Practices http://github.com/ginader/Accessible-Tabs/issues/1/find
- * * 1.6 
+ * * 1.6
  * * * new option "saveState" to allow tabs remember their selected state using cookies requires the cookie plugin: http://plugins.jquery.com/project/Cookie
  * * * changed supported jquery version to 1.4.2 to make sure it's future compatible
  * * * new option "autoAnchor" which allows to add ID's to headlines in the tabs markup that allow direct linking into a tab i.e.: file.html#headlineID
@@ -44,9 +44,9 @@
      <li class="next"><a href="#{the-id-of-the-next-tab}"><span>{the headline of the previous tab}</span></a></li>
  </ul>
  * * 1.8
- * * * new option "position" can be 'top' or 'bottom'. Defines where the tabs list is inserted. 
+ * * * new option "position" can be 'top' or 'bottom'. Defines where the tabs list is inserted.
  * * 1.8.1
- * * * Bugfix for broken pagination in ie6 and 7: Selector and object access modified by Daniel KÃ¶ntÃ¶s (www.MilkmanMedia.de). Thanks to Carolin Moll for the report.
+ * * * Bugfix for broken pagination in ie6 and 7: Selector and object access modified by Daniel Köntös (www.MilkmanMedia.de). Thanks to Carolin Moll for the report.
  * * 1.8.2
  * * * Bugfix for issue described by Sunshine here: http://blog.ginader.de/archives/2009/02/07/jQuery-Accessible-Tabs-How-to-make-tabs-REALLY-accessible.php#c916
  * * 1.8.3
@@ -54,9 +54,16 @@
  * * 1.9
  * * * new method showAccessibleTabSelector({valid jQuery selector of the tab to show}) that allows the opening of tabs \
  * * * by jQuery Selector instead of the index in showAccessibleTab() fixing issue https://github.com/ginader/Accessible-Tabs/issues/15
- * * 1.9.1 by Michael Schulze: 
+ * * 1.9.1 by Michael Schulze:
  * * * firstNavItemClass and lastNavItemClass to define a custom classname on the first and last tab
  * * * wrapInnerNavLinks: inner wrap for a-tags in tab navigation.
+ * * 1.9.2
+ * * * Bugfix by Dirk Jesse: fixing an issue that happened when passing multiple selectors to the init call instead of one
+ * * * Bugfix that fixes a reset of the tabs counter when accessibleTabs() was called more than once on a page
+ * * 1.9.3
+ * * * Bugfix by Norm: before, when cssClassAvailable was true, all of the tabhead elements had to have classes or they wouldn't get pulled out into tabs.
+ * * * This commit fixes this assumption, as I only want classes on some elements https://github.com/ginader/Accessible-Tabs/pull/25
+ * * 1.9.4 Bugfix by Patrick Bruckner to fix issue with Internet Explorer using jQuery 1.7 https://github.com/ginader/Accessible-Tabs/issues/26
  */
 
 
@@ -73,7 +80,7 @@
             var defaults = {
                 wrapperClass: 'content', // Classname to apply to the div that is wrapped around the original Markup
                 currentClass: 'current', // Classname to apply to the LI of the selected Tab
-                tabhead: 'h3', // Tag or valid Query Selector of the Elements to Transform the Tabs-Navigation from (originals are removed)
+                tabhead: 'h4', // Tag or valid Query Selector of the Elements to Transform the Tabs-Navigation from (originals are removed)
                 tabheadClass: 'tabhead', // Classname to apply to the target heading element for each tab div
                 tabbody: '.tabbody', // Tag or valid Query Selector of the Elements to be treated as the Tab Body
                 fx:'show', // can be "fadeIn", "slideDown", "show"
@@ -96,7 +103,7 @@
             var keyCodes = {
                 37 : -1, //LEFT
                 38 : -1, //UP
-                39 : +1, //RIGHT 
+                39 : +1, //RIGHT
                 40 : +1 //DOWN
             };
             var positions = {
@@ -104,56 +111,78 @@
                 bottom : 'append'
             };
             this.options = $.extend(defaults, config);
+
+            var tabsCount = 0;
+            if($("body").data('accessibleTabsCount') !== undefined){
+                tabsCount = $("body").data('accessibleTabsCount');
+            }
+            $("body").data('accessibleTabsCount',this.size()+tabsCount);
+
             var o = this;
             return this.each(function(t) {
                 var el = $(this);
                 var list = '';
                 var tabCount = 0;
-                ids = [];
-                
+                var ids = [];
+
                 $(el).wrapInner('<div class="'+o.options.wrapperClass+'"></div>');
-                
+
                 $(el).find(o.options.tabhead).each(function(i){
                     var id = '';
                     elId = $(this).attr('id');
                     if(elId){
+                        // Skip this item if it already exists.
+                        if(elId.indexOf('accessibletabscontent') === 0) {
+                            return;
+                        }
                         id =' id="'+elId+'"';
                     }
-                    var tabId = o.getUniqueId('accessibletabscontent', t, i);//get a unique id to assign to this tab's heading
-                    var navItemId = o.getUniqueId('accessibletabsnavigation', t, i);//get a unique id for this navigation item
+                    var tabId = o.getUniqueId('accessibletabscontent', tabsCount+t, i);//get a unique id to assign to this tab's heading
+                    var navItemId = o.getUniqueId('accessibletabsnavigation', tabsCount+t, i);//get a unique id for this navigation item
                     ids.push(tabId);
                     if(o.options.cssClassAvailable === true) {
                         var cssClass = '';
                         if($(this).attr('class')) {
                             cssClass = $(this).attr('class');
                             cssClass = ' class="'+cssClass+'"';
-                            list += '<li id="'+navItemId+'"><a'+id+''+cssClass+' href="#'+tabId+'">'+$(this).html()+'</a></li>';
                         }
+                        list += '<li id="'+navItemId+'"><a'+id+''+cssClass+' href="#'+tabId+'">'+$(this).html()+'</a></li>';
                     } else {
                       list += '<li id="'+navItemId+'"><a'+id+' href="#'+tabId+'">'+$(this).html()+'</a></li>';
                     }
                     $(this).attr({"id": tabId, "class": o.options.tabheadClass, "tabindex": "-1"});//assign the unique id and the tabheadClass class name to this tab's heading
                     tabCount++;
                 });
-                
+
                 if (o.options.syncheights && $.fn[o.options.syncHeightMethodName]) {
                     $(el).find(o.options.tabbody)[o.options.syncHeightMethodName]();
-                    $(window).resize(function(){ 
+                    $(window).resize(function(){
                         $(el).find(o.options.tabbody)[o.options.syncHeightMethodName]();
                     });
                 }
-                
-                $(el)[positions[o.options.position]]('<ul class="clearfix '+o.options.tabsListClass+' tabamount'+tabCount+'">'+list+'</ul>');
-                $(el).find(o.options.tabbody).hide();
-                $(el).find(o.options.tabbody+':first').show();
+
+                // Ensure that the call to setup tabs is re-runnable
+                var tabs_selector = '.' + o.options.tabsListClass;
+                if(!$(el).find(tabs_selector).length) {
+                    $(el)[positions[o.options.position]]('<ul class="clearfix '+o.options.tabsListClass+' tabamount'+tabCount+'"></ul>');
+                }
+
+                $(el).find(tabs_selector).append(list);
+
+                // initial show first content block and hide the others
+                var content = $(el).find(o.options.tabbody);
+                if (content.length > 0) {
+                    $(content).hide();
+                    $(content[0]).show();
+                }
                 $(el).find("ul."+o.options.tabsListClass+">li:first").addClass(o.options.currentClass).addClass(o.options.firstNavItemClass)
                   .find('a')[o.options.currentInfoPosition]('<span class="'+o.options.currentInfoClass+'">'+o.options.currentInfoText+'</span>')
                   .parents("ul."+o.options.tabsListClass).children('li:last').addClass(o.options.lastNavItemClass);
-                
+
                 if (o.options.wrapInnerNavLinks) {
                   $(el).find('ul.'+o.options.tabsListClass+'>li>a').wrapInner(o.options.wrapInnerNavLinks);
                 }
-                
+
                 $(el).find('ul.'+o.options.tabsListClass+'>li>a').each(function(i){
                     $(this).click(function(event){
                         event.preventDefault();
@@ -169,22 +198,22 @@
                         $(this)[o.options.currentInfoPosition]('<span class="'+o.options.currentInfoClass+'">'+o.options.currentInfoText+'</span>')
                         .parent().addClass(o.options.currentClass);
                         //now, only after writing the currentInfoText span to the tab list link, set focus to the tab's heading
-                        $($(this).attr("href")).focus().keyup(function(event){
+                        $($(this).attr("href"),true).focus().keyup(function(event){
                             if(keyCodes[event.keyCode]){
                                 o.showAccessibleTab(i+keyCodes[event.keyCode]);
                                 $(this).unbind( "keyup" );
                             }
                         });
-                        
+
                         // $(el).find('.accessibletabsanchor').keyup(function(event){
                         //     if(keyCodes[event.keyCode]){
                         //         o.showAccessibleTab(i+keyCodes[event.keyCode]);
                         //     }
                         // });
-                        
-                        
+
+
                     });
-                    
+
                     $(this).focus(function(event){
                         $(document).keyup(function(event){
                             if(keyCodes[event.keyCode]){
@@ -195,9 +224,9 @@
                     $(this).blur(function(event){
                         $(document).unbind( "keyup" );
                     });
-                    
+
                 });
-                
+
                 if(o.options.saveState && $.cookie){
                     var savedState = $.cookie('accessibletab_'+el.attr('id')+'_active');
                     debug($.cookie('accessibletab_'+el.attr('id')+'_active'));
@@ -205,14 +234,14 @@
                         o.showAccessibleTab(savedState,el.attr('id'));
                     }
                 }
-                
+
                 if(o.options.autoAnchor && window.location.hash){
                     var anchorTab = $('.'+o.options.tabsListClass).find(window.location.hash);
                     if(anchorTab.size()){
                         anchorTab.click();
                     }
                 }
-                
+
                 if(o.options.pagination){
                     var m = '<ul class="pagination">';
                     m +='    <li class="previous"><a href="#{previousAnchor}"><span>{previousHeadline}</span></a></li>';
--- html/yaml/core/js/yaml-focusfix.js
+++ html/yaml/core/js/yaml-focusfix.js
@@ -1,32 +1,32 @@
 /**
- * "Yet Another Multicolumn Layout" - (X)HTML/CSS Framework
+ * "Yet Another Multicolumn Layout" - YAML CSS Framework
  *
  * (en) Workaround for IE8 und Webkit browsers to fix focus problems when using skiplinks
  * (de) Workaround für IE8 und Webkit browser, um den Focus zu korrigieren, bei Verwendung von Skiplinks
  *
- * @note			inspired by Paul Ratcliffe's article 
+ * @note			inspired by Paul Ratcliffe's article
  *					http://www.communis.co.uk/blog/2009-06-02-skip-links-chrome-safari-and-added-wai-aria
  *                  Many thanks to Mathias Schäfer (http://molily.de/) for his code improvements
  *
- * @copyright       Copyright 2005-2011, Dirk Jesse
- * @license         CC-A 2.0 (http://creativecommons.org/licenses/by/2.0/),
- *                  YAML-C (http://www.yaml.de/en/license/license-conditions.html)
+ * @copyright       Copyright 2005-2012, Dirk Jesse
+ * @license         CC-BY 2.0 (http://creativecommons.org/licenses/by/2.0/),
+ *                  YAML-CDL (http://www.yaml.de/license.html)
  * @link            http://www.yaml.de
  * @package         yaml
- * @version         3.3.1
- * @revision        $Revision: 501 $
- * @lastmodified    $Date: 2011-06-18 17:27:44 +0200 (Sa, 18 Jun 2011) $
+ * @version         4.0+
+ * @revision        $Revision: 617 $
+ * @lastmodified    $Date: 2012-01-05 23:56:54 +0100 (Do, 05 Jan 2012) $
  */
- 
+
 (function () {
-	var YAML_focusFix = { 
-		skipClass : 'skip',
-		
+	var YAML_focusFix = {
+		skipClass : 'ym-skip',
+
 		init : function () {
 			var userAgent = navigator.userAgent.toLowerCase();
 			var	is_webkit = userAgent.indexOf('webkit') > -1;
 			var	is_ie = userAgent.indexOf('msie') > -1;
-			
+
 			if (is_webkit || is_ie) {
 				var body = document.body,
 					handler = YAML_focusFix.click;
@@ -37,26 +37,26 @@
 				}
 			}
 		},
-		
+
 		trim : function (str) {
 			return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 		},
-		
+
 		click : function (e) {
 			e = e || window.event;
 			var target = e.target || e.srcElement;
 			var a = target.className.split(' ');
-			
+
 			for (var i=0; i < a.length; i++) {
 				var cls = YAML_focusFix.trim(a[i]);
 				if ( cls === YAML_focusFix.skipClass) {
 					YAML_focusFix.focus(target);
-					break;			
+					break;
 				}
 			}
 		},
-		
-		focus : function (link) {		
+
+		focus : function (link) {
 			if (link.href) {
 				var href = link.href,
 					id = href.substr(href.indexOf('#') + 1),
@@ -64,7 +64,7 @@
 				if (target) {
 					target.setAttribute("tabindex", "-1");
 					target.focus();
-				}				
+				}
 			}
 		}
 	};
